25238cb91006a23f4aa128a93a3f317b49315fbe
mjcvaz <mario.j.vaz@gmail.com>
2024-05-06 23:39:35 +0100

    mjcv

--- python-001/hello.py
+++ python-001/hello.py
@@ -1,1 +1,20 @@
-teste
+#!/usr/bin/python
+
+import datetime
+import os
+import sys
+import time
+
+now = datetime.datetime.now()
+
+filename = "%s_%.2i-%.2i-%i_%.2i-%.2i-%.2i" % ('ping',now.day,now.month,now.year,now.hour,now.minute,now.second)
+
+print (filename)
+
+data = datetime.datetime.now().strftime("%Y-%m-%d")
+hora = datetime.datetime.now().strftime("%H:%M:%S")
+
+registo = data + ":" + hora
+print (registo)
+
+#fim
