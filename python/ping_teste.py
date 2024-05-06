ac7fda48dded9d96966593972d14464d3d9848a2
mjcvaz <mario.j.vaz@gmail.com>
2024-05-06 20:31:53 +0100

    mjcv

--- python/ping_teste.py
+++ python/ping_teste.py
@@ -8,18 +8,20 @@ import time
 now = datetime.datetime.now()
 
 #path_prefix = "/usr/local/logs/scripts/python/cisco_cfg/"
-path_prefix = 'C:\Users\884103\github\mjcv-projects\python\'
-filename = "%s_%.2i-%.2i-%i_%.2i-%.2i-%.2i" % ('ping',now.day,now.month,now.year,now.hour,now.minute,now.second)
-output_file = path_prefix + filename
+#path_prefix = 'C:\Users\884103\github\mjcv-projects\python\'
+#filename = "%s_%.2i-%.2i-%i_%.2i-%.2i-%.2i" % ('ping',now.day,now.month,now.year,now.hour,now.minute,now.second)
+#output_file = path_prefix + filename
+#output_file = 'C:\Users\884103\github\mjcv-projects\python\a.txt'
 
-hosts = "C:\Users\884103\github\mjcv-projects\python\hosts_imps.txt"
-hostsFile = open(hosts, "r")
-lines = hostsFile.readlines()
-fp=open(output_file,"w")
+#hosts = "C:\Users\884103\github\mjcv-projects\python\hosts_imps.txt"
+#hostsFile = open(hosts, "r")
+#lines = hostsFile.readlines()
+#fp=open(output_file,"w")
 
-os.system("clear")
-mine = []
+#os.system("clear")
+#mine = []
 
+lines = "10.1.1.1:a:b"
 for linha in lines:
     #print (linha)
     ip = linha
@@ -32,17 +34,17 @@ for linha in lines:
         #registo = ip + ":" + codigo + ":" + loja + ":1:" + data + ":" + hora
         registo = ip + ":" + data + ":" + hora
         print (registo)
-        mine.append(registo)
+        #mine.append(registo)
 
-        os.system("clear")
+        #os.system("clear")
         for j in mine:
             #ip, codigo, loja, st, data, h, m, s = j.split(":", 8)
             ip, st, data, h, m, s = j.split(":", 6)
             out = "%s%s-%s%s%s %s:%s:%s%s" % ("O IP ", ip, " em ", data, h, m, s, " is up")
             #out = "%s%s%s" % ("O IP ", j.rstrip() , " is up")
             print (out)
-fp.write(out + "\n")
+#fp.write(out + "\n")
 
-fp.close()
+#fp.close()
 
 #fim
